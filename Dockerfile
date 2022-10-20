474234392ac7bae43495aaeaf63f93b261b71a56
epicseven-cup <jllin3@buffalo.edu>
2022-10-20 19:42:16 -0400

    commmmit

--- Dockerfile
+++ Dockerfile
@@ -6,5 +6,5 @@ RUN pip install -r requirements.txt
 EXPOSE 8000
 ADD https://github.com/ufoscout/docker-compose-wait/releases/download/2.2.1/wait /wait
 RUN chmod +x /wait
-CMD /wait && python -u -m flask --app app run --host=0.0.0.0 -p 8000
+CMD /wait && python -u -m flask --app app --debug run --host=0.0.0.0 -p 8000
 
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 print("App running I think")
 
@@ -6,8 +6,19 @@ app = Flask("blue_Jesse")
 
 @app.route("/")
 def homepage():
-    print("I'm here")
-    return "<h1>Welcome!</h1>"
+    print("Someone is at the homepage")
+    return render_template("data/index.html")
+
+@app.route("/login")
+def loginPage():
+    return render_template("data/login.html") # Files can be served easier with static files check flask documenation
+
+
+
+
+
+
 
 
 # app.run() # Don't use this for final product [#Jacky]
+
