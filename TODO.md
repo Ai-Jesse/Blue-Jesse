8bf8ea87c278cd7c5cd1f115f15428fa0e35f1ce
epicseven-cup <jllin3@buffalo.edu>
2022-10-22 18:57:51 -0400

    1 hour work

---
+++ TODO.md
@@ -0,0 +1,3 @@
+# Task todo for Jacky
+
+### implantment a changelog by using template and database for the index.html of the front page
--- app.py
+++ app.py
@@ -4,14 +4,23 @@ print("App running I think")
 
 app = Flask("blue_Jesse")
 
-@app.route("/")
-def homepage():
+@app.route("/") # converts normal function to view function
+def homepage(): # view function
     print("Someone is at the homepage")
-    return render_template("data/index.html")
+    return render_template("index.html")
+
+    '''
+    When calling render_templates it search for the html files in the template folder
+    you can pass in a key value pair to override the value at the localtion of the html template example:
+    ----html----
+    e.g. {{test}}
+    ----html----
+    render_template(html, test=5) [#Jacky]
+    '''
 
 @app.route("/login")
 def loginPage():
-    return render_template("data/login.html") # Files can be served easier with static files check flask documenation
+    return render_template("login.html") # Files can be served easier with static files check flask documenation
 
 
 
