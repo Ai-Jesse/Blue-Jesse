1d630305dcb628b744041e69a7be4335bf797d4b
swatisur <swatisur@buffalo.edu>
2022-11-08 10:03:12 -0500

    dummy frontend

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from API import MongoDB_wrapper
 from flask import Flask, render_template
-
-
 from pymongo import MongoClient
 
 
@@ -16,8 +14,18 @@ print("App running I think")
 @app.route("/") # converts normal function to view function
 def homepage(): # view function
     print("Someone is at the homepage", flush=True)
-    return render_template("index.html")
+    return render_template("index.html", input="/login", input2="/signup")
 
+
+# @app.route("/login") # converts normal function to view function
+# def login(): # view function
+#     print("Someone is at the homepage", flush=True)
+#     return render_template("login.html", input="/login")
+
+@app.route("/signup") # converts normal function to view function
+def login(): # view function
+    print("Someone is at the homepage", flush=True)
+    return render_template("signup.html", input="/login")
     '''
     When calling render_templates it search for the html files in the template folder
     you can pass in a key value pair to override the value at the localtion of the html template example:
@@ -29,7 +37,7 @@ def homepage(): # view function
 
 @app.route("/login", methods=["GET"]) # Only get method
 def loginPage():
-    return render_template("login.html") # Files can be served easier with static files check flask documenation
+    return render_template("login.html", input="/signup") # Files can be served easier with static files check flask documenation
 
 
 @app.route("/loginData", methods=["POST"]) # Only post method
