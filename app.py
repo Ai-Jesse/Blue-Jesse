a68226effa6c9f49c614a980235b5ac36d0f1fef
swatisur <swatisur@buffalo.edu>
2022-11-24 12:33:31 -0500

    #4 CSS for index.html

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from API import MongoDB_wrapper
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pymongo import MongoClient
 
 
 # Setting up Database for the project
 client = MongoClient("mongo")
 mongo = MongoDB_wrapper(client)
+css_file = "static/style.css"
+
 print("mongoDB is up I think")
 app = Flask("blue_Jesse")
 print("App running I think")
@@ -14,18 +16,17 @@ print("App running I think")
 @app.route("/") # converts normal function to view function
 def homepage(): # view function
     print("Someone is at the homepage", flush=True)
-    return render_template("index.html", input="/login", input2="/signup")
+    return render_template("index.html", input="/login", input2="/signup", input3="/homepage", input4=css_file)
 
-
-# @app.route("/login") # converts normal function to view function
-# def login(): # view function
-#     print("Someone is at the homepage", flush=True)
-#     return render_template("login.html", input="/login")
+@app.route("/homepage") # converts normal function to view function
+def home(): # view function
+    print("Someone is at the userpage", flush=True)
+    return render_template("homepage.html", input="/homepage")
 
 @app.route("/signup") # converts normal function to view function
 def login(): # view function
     print("Someone is at the homepage", flush=True)
-    return render_template("signup.html", input="/login")
+    return render_template("signup.html", input="/login", input2="/signUpData")
     '''
     When calling render_templates it search for the html files in the template folder
     you can pass in a key value pair to override the value at the localtion of the html template example:
@@ -37,15 +38,20 @@ def login(): # view function
 
 @app.route("/login", methods=["GET"]) # Only get method
 def loginPage():
-    return render_template("login.html", input="/signup") # Files can be served easier with static files check flask documenation
+    return render_template("login.html", input="/signup", input2="/loginData") # Files can be served easier with static files check flask documenation
 
 
 @app.route("/loginData", methods=["POST"]) # Only post method
 def logging_userData():
-    print(request) # User name should be max 12 characters
+    r=request.form # User name should be max 12 characters
+    print(r)
     # special characters that we don't want in username: &, ~, /, <, >, ;, [space]
     
-
+@app.route("/signUpData", methods=["POST"]) # Only post method
+def signingup_userData():
+    print(request) # User name should be max 12 characters
+    # special characters that we don't want in username: &, ~, /, <, >, ;, [space]
+ 
 
 
 
